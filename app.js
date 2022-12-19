0727938da955fdf6bdd1943b11a4e3eb76786f3c
M2KV <wgmin.it@outlook.com>
2022-12-19 20:06:32 +0700

    feat: create base layout & update: bootstrap

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'hbs');
 app.use(mongoSanitize());
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/assets', [express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css/')), express.static(path.join(__dirname, 'node_modules/jquery/dist/'))]);
+app.use('/assets', [express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css/')), express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js/')), express.static(path.join(__dirname, 'node_modules/jquery/dist/'))]);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
