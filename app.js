add283c3d5dfe30359dfad2e6008d7252f53b9a2
M2KV <wgmin.it@outlook.com>
2022-12-20 18:27:21 +0700

    update partials

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
 import livereload from "livereload";
 import connectLiveReload from "connect-livereload";
-import express from 'express';
-import handlebars from 'express-handlebars';
-import mongoSanitize from 'express-mongo-sanitize';
-import bodyParser from 'body-parser';
-import HomeRoutes from './routes/HomeRoutes.js';
+import express from "express";
+import handlebars from "express-handlebars";
+import mongoSanitize from "express-mongo-sanitize";
+import bodyParser from "body-parser";
+import HomeRoutes from "./routes/HomeRoutes.js";
 
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const liveReloadServer = livereload.createServer();
 liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 10);
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 10);
 });
 
 const app = express();
 
 app.use(connectLiveReload());
 
-app.engine('.hbs', handlebars.engine({ defaultLayout: 'default', extname: '.hbs' }));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine(
+    ".hbs",
+    handlebars.engine({
+        defaultLayout: "default",
+        extname: "hbs",
+        partialsDir: path.join(__dirname, "views/partials/")
+    })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 app.use(mongoSanitize());
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/assets', [express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css/')), express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js/')), express.static(path.join(__dirname, 'node_modules/jquery/dist/'))]);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/assets", [
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/css/")),
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/js/")),
+    express.static(path.join(__dirname, "node_modules/jquery/dist/"))
+]);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', HomeRoutes);
+app.use("/", HomeRoutes);
 
 export default app;
