c365cf3a2910109ce8d891f536b5aa73286cd92a
thoaikhoa14402 <nguyenthoaidangkhoa@gmail.com>
2022-12-26 20:50:45 +0700

    using multer to upload file

--- routes/InstructorRoutes.js
+++ routes/InstructorRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.route('/my-courses').get(instructorController.getMyCourses);
 router.route('/add-course-description').get(instructorController.addCourseDescription);
 router.route('/add-course-content').get(instructorController.addCourseContent);
-
+router.post('/upload-lesson', instructorController.uploadLesson);
 
 export default router;
