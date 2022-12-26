4a74174580b19eba55b1bc1bccebc31684d12987
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2022-12-26 13:28:03 +0700

    configure Admin UI

--- app.js
+++ app.js
@@ -38,13 +38,6 @@ liveReloadServer.server.once("connection", () => {
 
 app.use(connectLiveReload());
 
-// Handlebars.registerHelper('times', function(n, block) {
-//   var accum = '';
-//   for(var i = 0; i < n; ++i)
-//       accum += block.fn(i);
-//   return accum;
-// });
-
 
 app.engine(
   ".hbs",
