cc6ff9a60100a4da995385f30007d30247535e60
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2022-12-27 20:01:28 +0700

    course detail header UI

--- controllers/courseDetailController.js
+++ controllers/courseDetailController.js
@@ -1,16 +1,24 @@
 import catchAsync from "../utilities/catchAsync.js";
 import Course from "../models/courseModel.js";
+import courseDetail from "../models/courseDetailsModel.js";
 import url from "url";
 
 export const renderCourseDetail = catchAsync(async (req, res) => {
     const getCourse = await Course.findOne({
         slug: `/course${url.parse(req.url, true).pathname}/`,
     }).lean();
-    console.log("-----------------");
-    console.log(getCourse);
+
+
+    const getCourseRating = await courseDetail.findOne({
+        course_id: getCourse._id
+    }).lean();  
+
     res.render("courseDetail/courseDetail.hbs", {
         courseDetail: getCourse,
-        isBestSeller: getCourse.sale > 100
+        courseRating: getCourseRating,
+        integerPart: Math.floor(getCourseRating.avg_rating),
+        isRemainder: getCourseRating.avg_rating - Math.floor(getCourseRating.avg_rating) !==0,
+        dateUpdate: getCourse.date.slice(0,10)
     });
 
   });
