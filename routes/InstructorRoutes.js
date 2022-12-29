6085b866bbbb63e9608da3bf43ecc58b1aeccc15
thoaikhoa14402 <nguyenthoaidangkhoa@gmail.com>
2022-12-29 21:14:43 +0700

    Building add description for course (instructor) UI

--- routes/InstructorRoutes.js
+++ routes/InstructorRoutes.js
@@ -3,7 +3,7 @@ import * as instructorController from "../controllers/instructorController.js";
 const router = express.Router();
 
 router.route('/my-courses').get(instructorController.getMyCourses);
-router.route('/add-course-description').get(instructorController.addCourseDescription);
+router.route('/add-course-description').get(instructorController.renderCourseDescription);
 router.route('/add-course-content').get(instructorController.renderCourseContent).post(instructorController.editCourseContent);
 
 export default router;
