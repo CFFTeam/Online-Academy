6d4d97b66d32142352f9931a90fb4719705f52c0
khangduy017 <khangduy017@gmail.com>
2022-12-29 20:01:29 +0700

    wishlist: show course, link course detail

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -1,10 +1,36 @@
 import Course from "../models/courseModel.js";
 import User from "../models/userModel.js";
+import CourseDetails from "../models/courseDetailsModel.js";
 import catchAsync from "../utilities/catchAsync.js";
 
 
 export const wishlistPage = catchAsync(async (req, res) => {
   res.locals.handlebars = "wishlist/wishlist";
+  let user = null;
+  const courses = [];
+  if (res.locals && res.locals.authUser) {
+    user = await User.findOne({ _id: res.locals.authUser._id });
+  }
+  if (user) {
+    for (const m of user.wishlist) {
+      const course = await Course.findOne({ _id: m });
+      const courseDetails = await CourseDetails.findOne({ course_id: m }).lean();
+      courses.push({
+        id: m,
+        slug: course.slug,
+        name: course.name,
+        img: course.img,
+        author: course.author,
+        category: course.category,
+        date: course.date.slice(0, course.date.indexOf("T")),
+        price: course.price,
+        rate: courseDetails.avg_rating,
+        numReview: courseDetails.num_reviews,
+        view: courseDetails.viewer
+      });
+    }
+  }
 
-  res.render(res.locals.handlebars);
+  res.render(res.locals.handlebars, { courses: JSON.stringify(courses) });
+
 });
