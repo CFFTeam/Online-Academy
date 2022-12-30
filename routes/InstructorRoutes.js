f89f0a2da26310159d5bc1ebbd1d4b7a64198e3b
thoaikhoa14402 <nguyenthoaidangkhoa@gmail.com>
2022-12-30 12:30:44 +0700

    update sidebar UI  (instructor page)

--- routes/InstructorRoutes.js
+++ routes/InstructorRoutes.js
@@ -2,8 +2,13 @@ import express from "express";
 import * as instructorController from "../controllers/instructorController.js";
 const router = express.Router();
 
+router.route('/dashboard').get(instructorController.getDashboard);
+router.route('/enrolled-courses').get(instructorController.getEnrolledCourses);
+router.route('/watch-history').get(instructorController.getWatchHistory);
+router.route('/sales').get(instructorController.getSales);
+router.route('/my-profile').get(instructorController.getInstructorProfile);
 router.route('/my-courses').get(instructorController.getMyCourses);
-router.route('/add-course-description').get(instructorController.renderCourseDescription);
-router.route('/add-course-content').get(instructorController.renderCourseContent).post(instructorController.editCourseContent);
+router.route('/add-course-description').get(instructorController.getCourseDescription);
+router.route('/add-course-content').get(instructorController.getCourseContent).post(instructorController.editCourseContent);
 
 export default router;
