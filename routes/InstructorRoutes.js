9c441cb562214ef070e253434e1aacd67977cd0b
thoaikhoa14402 <nguyenthoaidangkhoa@gmail.com>
2023-01-07 03:50:18 +0700

    create protect middleware (validate user role)

--- routes/InstructorRoutes.js
+++ routes/InstructorRoutes.js
@@ -1,15 +1,16 @@
 import express from "express";
 import * as instructorController from "../controllers/instructorController.js";
+import { validateInstructor } from "../middlewares/validate_role.mdw.js";
 const router = express.Router();
 
-router.route('/dashboard').get(instructorController.getDashboard);
-router.route('/enrolled-courses').get(instructorController.getEnrolledCourses);
-router.route('/watch-history').get(instructorController.getWatchHistory);
-router.route('/sales').get(instructorController.getSales);
-router.route('/preview').get(instructorController.getPreview);
-router.route('/my-profile').get(instructorController.getInstructorProfile).post(instructorController.updateInstructorProfile);
-router.route('/my-courses').get(instructorController.getMyCourses);
-router.route('/add-course-description').get(instructorController.getCourseDescription).post(instructorController.editCourseDescription);
-router.route('/add-course-content').get(instructorController.getCourseContent).post(instructorController.editCourseContent);
+router.route('/dashboard').get(validateInstructor, instructorController.getDashboard);
+router.route('/enrolled-courses').get(validateInstructor, instructorController.getEnrolledCourses);
+router.route('/watch-history').get(validateInstructor, instructorController.getWatchHistory);
+router.route('/sales').get(validateInstructor, instructorController.getSales);
+router.route('/preview').get(validateInstructor, instructorController.getPreview);
+router.route('/my-profile').get(validateInstructor, instructorController.getInstructorProfile).post(validateInstructor, instructorController.updateInstructorProfile);
+router.route('/my-courses').get(validateInstructor, instructorController.getMyCourses);
+router.route('/add-course-description').get(validateInstructor, instructorController.getCourseDescription).post(validateInstructor, instructorController.editCourseDescription);
+router.route('/add-course-content').get(validateInstructor, instructorController.getCourseContent).post(validateInstructor, instructorController.editCourseContent);
 
 export default router;
