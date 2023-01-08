ab0a376184ba8c9ae5054ddc744a8a6f636a21d9
M2KV <wgmin.it@outlook.com>
2023-01-08 16:59:08 +0700

    fix carousel inactive course

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,10 +3,15 @@ import User from '../models/userModel.js';
 import courseDetailsModel from "../models/courseDetailsModel.js";
 import { fixDateFormat, fixNumberFormat } from "../utilities/fixFormat.js";
 import catchAsync from "../utilities/catchAsync.js";
+import Course from "../models/courseModel.js";
+import courseDetails from "../models/courseDetailsModel.js";
+import Category from '../models/categoryModel.js';
 
 const loadhotCourse = async (myCourses, myWishCourses, categories, authors) => { 
     const hotCourses = await courseDetailsModel.find({ viewer: { $gt: 40000 } }).select('-reviews').sort('-viewer').limit(10).lean();
     const newcourse = [];
+
+    let first = true;
 
     for (let index = 0; index < hotCourses.length; index++) {
         const hotCoursesDetails = hotCourses[index];
@@ -14,7 +19,7 @@ const loadhotCourse = async (myCourses, myWishCourses, categories, authors) => {
         if (!course) continue;
 
         const new_hot_course = {
-            active: index === 0 ? true : false,
+            active: first ? true : false,
             course_name: course.name,
             course_slug: course.slug,
             course_rate: hotCoursesDetails.avg_rating,
@@ -33,6 +38,7 @@ const loadhotCourse = async (myCourses, myWishCourses, categories, authors) => {
             my_courses: (myCourses && myCourses.includes(course._id.toString())) ? true : false,
             myWishCourses: (myWishCourses && myWishCourses.includes(course._id.toString())) ? "chosen" : ""
         }
+        first = false;
         newcourse.push(new_hot_course);
     }
     return newcourse;
@@ -87,9 +93,29 @@ const loadNewestCourse = async (myCourses, myWishCourses, categories, authors) =
 }
 
 const loadTopCategories = async (categories) => {
-    const hotCourses = await courseDetailsModel.find({ viewer: { $gt: 40000 } }, { course_id: 1 }).sort('-viewer').limit(10).lean();
+    const categoriesList = JSON.parse(categories);
+    const categoriesIDList = categoriesList.map((category) => category._id.toString());
 
-    
+    const topCategories = [];
+
+    for (let i = 0; i < categoriesIDList.length; i++) {
+        const course = await Course.find({ category: categoriesIDList[i], finish: 1, active: true }, { _id: 1 }).lean();
+        const course_IDList = course.map((course) => course._id.toString());
+        
+        const course_Details = await courseDetails.find({ course_id: { $in: course_IDList } }).select('viewer').lean();
+
+        const total = course_Details.reduce((total, course) => total + course.viewer, 0);
+
+        topCategories.push({
+            category_id: categoriesIDList[i],
+            total: total
+        });
+    }
+    const topCategoriesList = topCategories.sort((a, b) => b.total - a.total);
+    const topCategories_ID = topCategoriesList.map((category) => category.category_id).splice(0, 5);
+
+    const top5Categories = await Category.find({ _id: { $in: topCategories_ID } }).select('title slug').lean();
+    return top5Categories;
 }
 
 export const load_my_courses = catchAsync(async (req, res, next) => {
@@ -118,6 +144,10 @@ export const homePage = catchAsync(async (req, res) => {
     const hotCourse = mostviewCourse.slice(0, 4);
 
     const newestCourse = await loadNewestCourse(req.myCourses, req.myWishCourses, categories, authors);
+    const topCategories = await loadTopCategories(res.locals.categories);
 
-    res.render('home/home', { hotCourse, mostviewCourse, newestCourse });
+    res.render('home/home', { hotCourse, mostviewCourse, newestCourse, topCategories: {
+        colors: ['top1gradient', 'top2gradient', 'top3gradient', 'top4gradient', 'top5gradient'],
+        top: topCategories
+    } });
 });
